b0e208e9eae626046ace8363e901393eca64708c
ritsarr <daniil0330moroz@gmail.com>
2024-03-24 19:47:09 +0300

    Initial Commit

---
+++ longCallculation/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  longCallculation
+//
+//  Created by Даниил on 24.03.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
